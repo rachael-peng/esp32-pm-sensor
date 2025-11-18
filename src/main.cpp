85e92f30f459d738c9c0dde801153db9799e9c15
rachael-peng <rachael-peng@users.noreply.github.com>
2025-11-17 22:56:38 -0800

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+// Pins: PM Sensor Pin ( TX - GPIO 17, RX - GPIO 16
+
 #include <Arduino.h>
 #include <Adafruit_PM25AQI.h> 
 
@@ -7,15 +9,16 @@ void setup() {
   // Wait for serial monitor to open
   Serial.begin(115200);
   while (!Serial) delay(10);
-
   Serial.println("Adafruit PMSA003I Air Quality Sensor");
+  
+  // Change default Serial1 pins (TX=GPIO 10, RX=GPIO 9) to safer pins:
+  // Serial.begin(BaudRate, Configuration, RX_Pin, TX_Pin)
+  Serial1.begin(9600, SERIAL_8N1, 16, 17); // Example: TX on GPIO 17, RX on GPIO 16
 
   // Wait three seconds for sensor to boot up!
   delay(3000);
 
-  Serial1.begin(9600);
-
-  // There are 3 options for connectivity!
+  // Connectivity:
   if (! aqi.begin_I2C()) {      // connect to the sensor over I2C
     if (! aqi.begin_UART(&Serial1)) { // connect to the sensor over hardware serial
       Serial.println("Could not find PM 2.5 sensor!");
@@ -36,23 +39,10 @@ void loop() {
   Serial.println("AQI reading success");
 
   Serial.println(F("---------------------------------------"));
-  Serial.println(F("Concentration Units (standard)"));
-  Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_standard);
-  Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_standard);
-  Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_standard);
-  Serial.println(F("---------------------------------------"));
   Serial.println(F("Concentration Units (environmental)"));
   Serial.print(F("PM 1.0: ")); Serial.print(data.pm10_env);
   Serial.print(F("\t\tPM 2.5: ")); Serial.print(data.pm25_env);
   Serial.print(F("\t\tPM 10: ")); Serial.println(data.pm100_env);
-  Serial.println(F("---------------------------------------"));
-  Serial.print(F("Particles > 0.3um / 0.1L air:")); Serial.println(data.particles_03um);
-  Serial.print(F("Particles > 0.5um / 0.1L air:")); Serial.println(data.particles_05um);
-  Serial.print(F("Particles > 1.0um / 0.1L air:")); Serial.println(data.particles_10um);
-  Serial.print(F("Particles > 2.5um / 0.1L air:")); Serial.println(data.particles_25um);
-  Serial.print(F("Particles > 5.0um / 0.1L air:")); Serial.println(data.particles_50um);
-  Serial.print(F("Particles > 10 um / 0.1L air:")); Serial.println(data.particles_100um);
-  Serial.println(F("---------------------------------------"));
   Serial.println(F("AQI"));
   Serial.print(F("PM2.5 AQI US: ")); Serial.print(data.aqi_pm25_us);
   Serial.print(F("\tPM10  AQI US: ")); Serial.println(data.aqi_pm100_us);
